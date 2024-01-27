7821613ff4074d9f781e7152c4d6435214d59be9
TAJAMUL HASSAN <tajamulhassan112@gmail.com>
2024-01-27 21:17:13 +0530

    Initial commit with Project On States files

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <div>
+      <nav className="bg-bgDark py-4 ">
+        <h1 className="text-center font-bold text-3xl text-white">Top Courses</h1>
+      </nav>
+    </div>
+  );
+}
+export default Navbar;
---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+import "./Spinner.css"
+function Spinner() {
+  return (
+    <div>
+      <div className='flex items-center flex-col space-y-2'>
+        <p className='text-lg text-bgDark font-semibold'>Loading...</p>
+         
+      </div>
+    </div>
+  )
+}
+
+export default Spinner
